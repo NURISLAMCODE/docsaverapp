3947d8c05d3901f4995938ab7d3a378128b21ffd
Nur Islam <info.nurislamrajib@gmail.com>
2024-07-06 21:25:57 +0600

    docsaverapp

---
+++ lib/screen/authentication_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatelessWidget {
+  static String routeName = "/authScreen";
+  const AuthScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
